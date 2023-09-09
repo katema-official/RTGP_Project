ecb70c3ad9949fa779f2da752f17588ccf92c675
Alessio La Greca <alessio.lagreca13@gmail.com>
2023-09-09 12:33:32 +0200

    Added GLM header files (not working rn)

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,26 @@
 #include <iostream>
 #include <tuple>
 #include <vector>
+#include <map>
+#include <string>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void processInput(GLFWwindow* window);
+void RenderText(Shader &shader, std::string text, float x, float y, float scale, glm::vec3 color){
+
+}
+
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1280;//800;
+const unsigned int SCR_HEIGHT = 720;//600;
 
 int CURRENT_VAO = 0;
 
@@ -91,7 +102,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "2DKPO_Execution_Visualizer", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -100,6 +111,9 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glfwSetKeyCallback(window, key_callback);
+
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -243,7 +257,34 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
     
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    /*if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        if(CURRENT_VAO == 0)
+        { 
+            CURRENT_VAO = 1;
+            std::cout << "Changed to 1" << std::endl;
+        }
+        else
+        { 
+            CURRENT_VAO = 0;
+            std::cout << "Changed to 0" << std::endl;
+        }
+    }*/
+
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_S && action == GLFW_PRESS)
     {
         if(CURRENT_VAO == 0)
         { 
@@ -256,14 +297,4 @@ void processInput(GLFWwindow* window)
             std::cout << "Changed to 0" << std::endl;
         }
     }
-
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
 }
